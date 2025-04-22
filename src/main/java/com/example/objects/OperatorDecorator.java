b4b38e83a4e2d9fe6f4f529044698d47fe5f13f9
nina2dev <nina2dev@gmail.com>
2025-04-22 19:01:53 -0400

    Remove tostring

--- src/main/java/com/example/objects/OperatorDecorator.java
+++ src/main/java/com/example/objects/OperatorDecorator.java
@@ -3,14 +3,4 @@ package com.example.objects;
 public abstract class OperatorDecorator extends Formula {
     protected Formula left;
     protected Formula right;
-
-    /**
-     * Returns a string representation of this operator.
-     *
-     * @return A string representation of this operator
-     */
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "{formula='" + getFormula() + "'}";
-    }
 }
