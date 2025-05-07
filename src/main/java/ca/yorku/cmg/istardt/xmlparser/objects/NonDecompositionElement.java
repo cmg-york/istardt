9014627b3c643795c531f0dc878585302de3f791
nina2dev <nina2dev@gmail.com>
2025-05-06 23:53:26 -0400

    Remove previous field in NonDecompositionElement

--- src/main/java/ca/yorku/cmg/istardt/xmlparser/objects/NonDecompositionElement.java
+++ src/main/java/ca/yorku/cmg/istardt/xmlparser/objects/NonDecompositionElement.java
@@ -2,14 +2,9 @@ package ca.yorku.cmg.istardt.xmlparser.objects;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
-/**
- * NonDecompositionElement class representing an element that cannot be further decomposed.
- */
 public abstract class NonDecompositionElement extends Element {
     @JacksonXmlProperty(localName = "formula")
     private Formula valueFormula;
-
-    private Boolean previous;
 
     public void setValueFormula(Formula valueFormula) {
         this.valueFormula = valueFormula;
@@ -17,13 +12,5 @@ public abstract class NonDecompositionElement extends Element {
 
     public Formula getFormula() {
         return valueFormula;
-    }
-
-    public Boolean getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(Boolean previous) {
-        this.previous = previous;
     }
 }
