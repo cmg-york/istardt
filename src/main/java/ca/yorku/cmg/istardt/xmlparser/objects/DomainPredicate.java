a41e0846c144478f5338d82a8695f243b5384a11
nina2dev <nina2dev@gmail.com>
2025-05-10 14:32:47 -0400

    Remove classes that extend Atom

--- src/main/java/ca/yorku/cmg/istardt/xmlparser/objects/DomainPredicate.java
+++
@@ -1,4 +0,0 @@
-package ca.yorku.cmg.istardt.xmlparser.objects;
-
-public class DomainPredicate extends Atom {
-}
--- src/main/java/ca/yorku/cmg/istardt/xmlparser/objects/Invariant.java
+++
@@ -1,4 +0,0 @@
-package ca.yorku.cmg.istardt.xmlparser.objects;
-
-public class Invariant extends Atom {
-}
