3edf248c28c062fbc3867478fc4d3a0f9751da2a
nina2dev <nina2dev@gmail.com>
2025-05-20 21:30:05 -0400

    Add BooleanConstant and NumericConstant

---
+++ src/main/java/ca/yorku/cmg/istardt/xmlparser/objects/BooleanConstant.java
@@ -0,0 +1,22 @@
+package ca.yorku.cmg.istardt.xmlparser.objects;
+
+public class BooleanConstant extends Formula {
+    private boolean content;
+
+    public BooleanConstant(boolean content){
+        this.content = content;
+    }
+
+    public boolean isContent() {
+        return content;
+    }
+
+    public void setContent(boolean content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getFormula() {
+        return String.valueOf(content);
+    }
+}
