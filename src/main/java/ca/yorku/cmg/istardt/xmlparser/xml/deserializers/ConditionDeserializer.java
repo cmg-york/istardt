26de1e04628b1a8facfea87bf9bafa4f5a9cd6ff
nina2dev <nina2dev@gmail.com>
2025-07-21 23:27:01 -0400

    Allow ConditionDeserializer to register the element

--- src/main/java/ca/yorku/cmg/istardt/xmlparser/xml/deserializers/ConditionDeserializer.java
+++ src/main/java/ca/yorku/cmg/istardt/xmlparser/xml/deserializers/ConditionDeserializer.java
@@ -1,15 +1,10 @@
 package ca.yorku.cmg.istardt.xmlparser.xml.deserializers;
 
-import ca.yorku.cmg.istardt.xmlparser.objects.Atom;
 import ca.yorku.cmg.istardt.xmlparser.objects.Condition;
-import ca.yorku.cmg.istardt.xmlparser.xml.utils.CustomLogger;
-import ca.yorku.cmg.istardt.xmlparser.xml.utils.DeserializerUtils;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
-
 import java.io.IOException;
-import java.util.UUID;
 
 public class ConditionDeserializer extends BaseDeserializer<Condition> {
     public ConditionDeserializer() {
@@ -19,23 +14,6 @@ public class ConditionDeserializer extends BaseDeserializer<Condition> {
     @Override
     protected Condition createNewElement() {
         return new Condition();
-    }
-
-    @Override
-    protected Condition extractCommonAttributes(Condition element, JsonNode node) {
-        String id = DeserializerUtils.getStringAttribute(node, "id", UUID.randomUUID().toString());
-        element.setId(id);
-
-        String name = DeserializerUtils.getStringAttribute(node, "name", "");
-        String description = DeserializerUtils.getStringAttribute(node, "description", "");
-
-        // Create atom and add bidirectional relationship
-        Atom atom = createAtom(name, description);
-        element.setRepresentation(atom);
-        atom.setElement(element);
-
-        // Do not add condition to elementsByName map
-        return element;
     }
 
     @Override
